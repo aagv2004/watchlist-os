db404fe0f609c1fdfa87f6aae74a60be9a22ea69
Alejandro Gonzalez Vergara <andresg.vergara04@gmail.com>
2026-01-31 01:15:11 -0300

    tmdb rutas dinámicas en el import

--- server/routes/tmdbRoutes.js
+++ server/routes/tmdbRoutes.js
@@ -1,16 +1,13 @@
 import express from "express";
-import { searchContent } from "../controllers/tmdbController.js";
+import {
+  searchContent,
+  getSeasonDetailsController,
+} from "../controllers/tmdbController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/search", protect, searchContent);
-router.get(
-  "/series/:id/season/:season",
-  protect,
-  import("../controllers/tmdbController.js").then(
-    (m) => m.getSeasonDetailsController,
-  ),
-);
+router.get("/series/:id/season/:season", protect, getSeasonDetailsController);
 
 export default router;
